226df74e45ac1ab18bdd0045b0e04ed0d5ec6a20
keei051 <keei@inbox.ru>
2025-07-09 02:41:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные окружения из .env
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 VK_TOKEN = os.getenv("VK_TOKEN")
 
-if not BOT_TOKEN or not VK_TOKEN:
-    raise ValueError("Переменные окружения BOT_TOKEN и VK_TOKEN должны быть заданы в .env")
+# Настройки
+MAX_LINKS_PER_BATCH = 50
 
-# Ограничения
-MAX_LINKS_PER_BATCH = 50  # Максимум ссылок за одну массовую загрузку
+if not BOT_TOKEN or not VK_TOKEN:
+    raise ValueError("BOT_TOKEN или VK_TOKEN не найдены в .env")
