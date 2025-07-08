506d02c53b6cec4bc8f4edebeabd4e278f084962
keei051 <keei@inbox.ru>
2025-07-09 02:39:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-import logging
+# main.py
 import asyncio
-import os
+import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from dotenv import load_dotenv
-from handlers import router as handlers_router
+import os
+
+from handlers import router
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN is missing in .env")
-
-# Инициализация
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.MARKDOWN_V2)
-dp = Dispatcher()
-dp.include_router(handlers_router)
 logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.MARKDOWN)
+dp = Dispatcher()
+dp.include_router(router)
 
 async def main():
     await dp.start_polling(bot)
